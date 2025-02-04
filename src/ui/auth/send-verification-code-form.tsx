ca700545762bf9177a6d617542947be6fc7c39bd
vikesh <vikesh@cloudechosystems.com>
2025-02-04 12:42:49 +0530

    fix: unused removed

--- src/ui/auth/send-verification-code-form.tsx
+++ src/ui/auth/send-verification-code-form.tsx
@@ -1,27 +1,26 @@
 "use client";
 
-import { useFormState, useFormStatus } from "react-dom";
-import { Button } from "../button"
+import { useFormStatus } from "react-dom";
+import { Button } from "../button";
 import { handleSendEmailVerificationCode } from "@/lib/cognitoActions";
 import { useActionState } from "react";
 
-
 export default function SendVerificationCode() {
-    const [response, dispatch] = useActionState(handleSendEmailVerificationCode, {
-        message: "",
-        errorMessage: "",
-    });
-    
-    const { pending } = useFormStatus();
-    return (
-        <>
-            <Button
-                className="mt-4 w-full"
-                aria-disabled={pending} 
-                formAction={dispatch}
-            >
-                Resend Verification Code
-            </Button>            
-        </>
-    );
+  const [dispatch] = useActionState(handleSendEmailVerificationCode, {
+    message: "",
+    errorMessage: "",
+  });
+
+  const { pending } = useFormStatus();
+  return (
+    <>
+      <Button
+        className="mt-4 w-full"
+        aria-disabled={pending}
+        formAction={dispatch}
+      >
+        Resend Verification Code
+      </Button>
+    </>
+  );
 }
